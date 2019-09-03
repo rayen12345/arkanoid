2eaac6635af765d50d88b6a32f6a8c2f1545069a
rayen12345 <54586143+rayen12345@users.noreply.github.com>
2019-09-03 20:41:10 +0100

    Add files via upload

---
+++ Blocks1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks1 : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+
+        Destroy(gameObject);
+        Debug.Log("you won");
+
+
+
+    }
+
+}
---
+++ Racket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    public float speed = 150;
+    public string Axis;
+    void FixedUpdate()
+    {
+        // Get Horizontal Input
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // Set Velocity (movement direction * speed)
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
